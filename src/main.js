c97522c15e3e7c1da8870e6ef297b86bf23ec682
jay <k82410@yeah.net>
2020-04-27 21:18:33 +0800

    首页结构

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { PullRefresh, Lazyload } from 'vant'
+import ElementUI from 'element-ui'
 
 import './assets/styles/base.scss'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+Vue.use(PullRefresh)
+Vue.use(Lazyload, {
+  lazyComponent: true,
+  preload: 1.3
+})
 
 Vue.config.productionTip = false
 
